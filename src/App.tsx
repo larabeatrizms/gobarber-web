aaa5cdb9e8c025b535e6513b3dad96ae5f7a85d0
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-12 02:36:22 -0300

    unform, usability, tooltip and validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
+
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
